6362921b9fc927f8422e217ac67e60867020947d
ruqing <2207997703@qq.com>
2025-08-30 15:14:12 +0800

    back

--- themes/hugo-eureka/assets/css/postcss.config.js
+++ themes/hugo-eureka/assets/css/postcss.config.js
@@ -2,9 +2,7 @@ const themeDir = __dirname + "/../../";
 
 module.exports = {
   plugins: [
-    require("@tailwindcss/postcss-plugin")({
-		config: themeDir + "assets/css/tailwind.config.js"
-		}),
+    require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
     require("autoprefixer")({
       path: [themeDir],
     }),
